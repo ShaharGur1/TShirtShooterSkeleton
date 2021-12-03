90db290fc765bdd9aa037ddaf146ae1b2454013c
ShaharGur1 <92610491+ShaharGur1@users.noreply.github.com>
2021-12-03 15:45:25 -0800

    h

--- src/main/cpp/Shooter.cpp
+++ src/main/cpp/Shooter.cpp
@@ -1,1 +1,44 @@
 #include "Shooter.h"
+
+Shooter::Shooter()
+{
+    m_compressor1 = new WPI_TalonSRX(1);
+    m_compressor2 = new WPI_TalonSRX(11);
+    m_firing_valve = new frc::Solenoid(0, 2); // Solenoid
+}
+
+void Shooter::Init()
+{
+    m_compressor1->Set(0.0);
+    m_compressor2->Set(0.0);
+
+}
+
+void Shooter::Stop()
+{
+    m_compressor1->Set(0.0);
+    m_compressor2->Set(0.0);    
+    m_firing_valve->Set(false);
+}
+
+void Shooter::Go()
+{
+    m_compressor1->Set(0.0);
+    m_compressor2->Set(0.0);
+    m_firing_valve->Set(false);
+}
+
+void Shooter::Shoot()
+{
+    m_firing_valve->Set(true);
+}
+
+void Shooter::ResetCounter()
+{
+    
+}
+
+void Shooter::StateMachine()
+{
+
+}
---
+++ src/main/include/Shooter.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <ctre/Phoenix.h>
+#include <frc/WPILib.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+#include <chrono>
+
+class Shooter
+{
+private:
+     WPI_TalonSRX *m_compressor1;
+    WPI_TalonSRX *m_compressor2;
+    frc::Solenoid *m_firing_valve;
+    frc::Solenoid *m_emergency_valve;
+
+
+    std::chrono::duration<double> counter;
+    
+    std::chrono::time_point<std::chrono::steady_clock> start;
+
+    std::chrono::time_point<std::chrono::steady_clock> end;
+
+public:
+
+    enum States {
+        INIT, STOP, GO,
+        SHOOT
+    };
+
+    States current_state;
+    States last_state;
+
+    Shooter();
+    void Init();
+    void Stop();
+    void Go();
+    void Shoot();
+    void ResetCounter();
+    void StateMAchine();
+
+}
